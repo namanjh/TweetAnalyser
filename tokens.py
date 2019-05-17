fe0a964a19d026ecea7d4b24478ed1333ba2bdff
Narendra Nath Jha <36468378+namanjh@users.noreply.github.com>
2019-05-18 01:53:41 +0530

    Create tokens.py
    This code is for acessing the tokens in the twitterstream.py.
    
    You have to register into the twitter developer console and register a new application and then generate the tokens. These tokens can be used to acess the new application that you are going to create. Here we are going to just stream the twitter data and then perform sentiment analysis.

---
+++ tokens.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 15:13:21 2019
+
+@author: naman
+"""
+
+#variables that contain the user credentials to acess twitter API
+ACCESS_TOKEN = "#################################################"
+ACCESS_TOKEN_SECRET = ""#################################################""
+CONSUMER_KEY = ""#################################################""
+CONSUMER_SECRET = ""#################################################""
